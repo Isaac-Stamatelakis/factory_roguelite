fa84e77e8157148f26316906780a8c9ceb2f4ab1
Isaac-Stamatelakis <97195929+Isaac-Stamatelakis@users.noreply.github.com>
2024-05-14 14:04:22 -0600

    foldout buttons work for custom structure viewer

--- Assets/Editor/Cave/Structure/StructureEditorView.cs
+++ Assets/Editor/Cave/Structure/StructureEditorView.cs
@@ -10,6 +10,8 @@ public class StructureEditorView : Editor
 {
     private GUIStyle textAreaStyle;
     private bool[] foldoutStates;
+
+    private List<Dictionary<string,string>> variantData;
     void OnEnable()
     {
         textAreaStyle = new GUIStyle(EditorStyles.textArea)
@@ -21,65 +23,64 @@ public class StructureEditorView : Editor
     public override void OnInspectorGUI()
     {
         Structure structure = (Structure)target;
-
-        foldoutStates = new bool[structure.variants.Count];
+        EditorGUI.BeginChangeCheck ();
+		EditorGUILayout.GetControlRect (true, 16f, EditorStyles.foldout);
+		if (EditorGUI.EndChangeCheck ()) {
+			Debug.Log ("fold state updated");
+		}
         EditorGUILayout.LabelField("Structure Variants", EditorStyles.boldLabel);
-
+        if (variantData == null) {
+            getVariantData(structure);
+        }
         for (int i = 0; i < structure.variants.Count; i++)
         {
             EditorGUILayout.BeginHorizontal();
             foldoutStates[i] = EditorGUILayout.Foldout(foldoutStates[i], "Variant " + (i + 1), true);
             EditorGUILayout.EndHorizontal();
-            /*
             if (!foldoutStates[i]) {
                 continue;
             }
-            */
-            EditorGUILayout.BeginHorizontal();
-            EditorGUILayout.LabelField("Variant " + (i + 1), EditorStyles.boldLabel);
-            EditorGUILayout.EndHorizontal();
             EditorGUILayout.LabelField("Mutable", EditorStyles.boldLabel);
             structure.variants[i].Frequency = EditorGUILayout.IntField("Frequency", structure.variants[i].Frequency);
             EditorGUILayout.LabelField("Immutable", EditorStyles.boldLabel);
             EditorGUILayout.Vector2IntField("Size", structure.variants[i].Size);
             int height = 200;
-            WorldTileConduitData worldTileConduitData = null;
-            try {
-                worldTileConduitData = JsonConvert.DeserializeObject<WorldTileConduitData>(structure.variants[i].Data);
-            } catch (JsonSerializationException) {
-                
-            }
-            if (worldTileConduitData == null) {
+            
+            Dictionary<string,string> displayData = variantData[i];
+            if (displayData == null) {
                 EditorGUILayout.LabelField("Data Corrupted", EditorStyles.boldLabel);
                 continue;
             }
-            EditorGUILayout.LabelField("Base Tiles", EditorStyles.miniBoldLabel);
-            EditorGUILayout.TextArea(JsonConvert.SerializeObject(worldTileConduitData.baseData), textAreaStyle, GUILayout.MinHeight(height));
+            foreach (KeyValuePair<string,string> kvp in displayData) {
+                EditorGUILayout.LabelField(kvp.Key, EditorStyles.miniBoldLabel);
+                EditorGUILayout.TextArea(kvp.Value, textAreaStyle, GUILayout.MinHeight(height));
+            }
             
-            EditorGUILayout.LabelField("Background Tiles", EditorStyles.miniBoldLabel);
-            EditorGUILayout.TextArea(JsonConvert.SerializeObject(worldTileConduitData.backgroundData), textAreaStyle, GUILayout.MinHeight(height));
-            
-            EditorGUILayout.LabelField("Fluid Tiles", EditorStyles.miniBoldLabel);
-            EditorGUILayout.TextArea(JsonConvert.SerializeObject(worldTileConduitData.fluidData), textAreaStyle, GUILayout.MinHeight(height));
-            
-            EditorGUILayout.LabelField("Entities", EditorStyles.miniBoldLabel);
-            EditorGUILayout.TextArea(JsonConvert.SerializeObject(worldTileConduitData.entityData), textAreaStyle, GUILayout.MinHeight(height));
-            
-            EditorGUILayout.LabelField("Item Conduits", EditorStyles.miniBoldLabel);
-            EditorGUILayout.TextArea(JsonConvert.SerializeObject(worldTileConduitData.itemConduitData), textAreaStyle, GUILayout.MinHeight(height));
-            
-            EditorGUILayout.LabelField("Fluid Conduits", EditorStyles.miniBoldLabel);
-            EditorGUILayout.TextArea(JsonConvert.SerializeObject(worldTileConduitData.fluidConduitData), textAreaStyle, GUILayout.MinHeight(height));
-            
-            EditorGUILayout.LabelField("Energy Conduits", EditorStyles.miniBoldLabel);
-            EditorGUILayout.TextArea(JsonConvert.SerializeObject(worldTileConduitData.energyConduitData), textAreaStyle, GUILayout.MinHeight(height));
-            
-            EditorGUILayout.LabelField("Signal Conduits", EditorStyles.miniBoldLabel);
-            EditorGUILayout.TextArea(JsonConvert.SerializeObject(worldTileConduitData.signalConduitData), textAreaStyle, GUILayout.MinHeight(height));
-           
-            EditorGUILayout.LabelField("Matrix Conduits", EditorStyles.miniBoldLabel);
-            EditorGUILayout.TextArea(JsonConvert.SerializeObject(worldTileConduitData.matrixConduitData), textAreaStyle, GUILayout.MinHeight(height));
-            
+        }
+    }
+
+    private void getVariantData(Structure structure) {
+        foldoutStates = new bool[structure.variants.Count];
+        variantData = new List<Dictionary<string, string>>();
+        foreach (StructureVariant variant in structure.variants) {
+            WorldTileConduitData worldTileConduitData = null;
+            try {
+                worldTileConduitData = JsonConvert.DeserializeObject<WorldTileConduitData>(variant.Data);
+            } catch (JsonSerializationException) {
+                variantData.Add(null);
+                continue;
+            }
+            Dictionary<string,string> displayData = new Dictionary<string, string>();
+            displayData["Base Tiles"] = JsonConvert.SerializeObject(worldTileConduitData.baseData);
+            displayData["Background Tiles"] = JsonConvert.SerializeObject(worldTileConduitData.backgroundData);
+            displayData["Fluid Tiles"] = JsonConvert.SerializeObject(worldTileConduitData.fluidData);
+            displayData["Entities"] = JsonConvert.SerializeObject(worldTileConduitData.entityData);
+            displayData["Item Conduits"] = JsonConvert.SerializeObject(worldTileConduitData.itemConduitData);
+            displayData["Fluid Conduits"] = JsonConvert.SerializeObject(worldTileConduitData.fluidConduitData);
+            displayData["Energy Conduits"] = JsonConvert.SerializeObject(worldTileConduitData.energyConduitData);
+            displayData["Signal Conduits"] = JsonConvert.SerializeObject(worldTileConduitData.signalConduitData);
+            displayData["Matrix Conduits"] = JsonConvert.SerializeObject(worldTileConduitData.matrixConduitData);
+            variantData.Add(displayData);
         }
     }
 
