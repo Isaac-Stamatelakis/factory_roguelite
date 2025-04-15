15437b3e4425460da191f7bde990357cb06925c1
Isaac-Stamatelakis <97195929+Isaac-Stamatelakis@users.noreply.github.com>
2025-04-15 09:26:46 -0600

    Fixed bug where image backgrounds were black

--- Assets/Scripts/DevTools/ItemImageGenerator/ItemImageGenerator.cs
+++ Assets/Scripts/DevTools/ItemImageGenerator/ItemImageGenerator.cs
@@ -71,6 +71,7 @@ namespace DevTools.ItemImageGenerator
             
                 mCaptureCamera.Render();
                 RenderTexture.active = renderTexture;
+                
                 Texture2D tex = new Texture2D(renderTexture.width, renderTexture.height, TextureFormat.RGBA32, false);
                 tex.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
                 tex.Apply();
