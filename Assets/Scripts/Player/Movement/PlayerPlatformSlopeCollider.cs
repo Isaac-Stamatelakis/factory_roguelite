ed59891d2733e1445f35456c3d949f3e366015d7
Isaac-Stamatelakis <97195929+Isaac-Stamatelakis@users.noreply.github.com>
2025-05-22 23:49:42 -0600

    Sloped platform detection now works perfectly. Right slope collisions implemented completely seperated from left slope collisions

--- Assets/Scripts/Player/Movement/PlayerPlatformSlopeCollider.cs
+++ Assets/Scripts/Player/Movement/PlayerPlatformSlopeCollider.cs
@@ -13,77 +13,81 @@ namespace Player.Movement
     {
         private PlayerRobot playerRobot;
         private Rigidbody2D rb;
-        private BoxCollider2D boxCollider2D;
-        public BoxCollider2D PlatformCollider;
-        public float MaxY = 0;
-        public Vector2 MaxSize;
-        public float MinActiveSpeed = -10;
-        public float MaxActiveSpeed = -20;
-        public Vector2 MaxColliderSize;
-        private Vector2 minColliderSize;
         public SlopeRotation SlopeDirection;
-        private Vector2 minSize;
-        private float minYPosition;
+        private CollisionState collisionState;
+        
         public void Start()
         {
             playerRobot = GetComponentInParent<PlayerRobot>();
             rb = playerRobot.GetComponent<Rigidbody2D>();
-            boxCollider2D = GetComponent<BoxCollider2D>();
-            minYPosition = transform.localPosition.y;
-
-            minSize = boxCollider2D.size;
-            minColliderSize = PlatformCollider.size;
-            
+            switch (SlopeDirection)
+            {
+                case SlopeRotation.Left:
+                    collisionState = CollisionState.OnLeftSlopePlatform;
+                    break;
+                case SlopeRotation.Right:
+                    collisionState = CollisionState.OnRightSlopePlatform;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
         }
-
-        public void Update()
-        {
-            return;
-            float yVelocity = rb.velocity.y;
-            float t = yVelocity > MinActiveSpeed ? 0 : yVelocity / MaxActiveSpeed;
-            var vector3 = transform.localPosition;
-            vector3.y = Mathf.Lerp(minYPosition, MaxY, t);
-            transform.localPosition = vector3;
-            boxCollider2D.size = Vector2.Lerp(minSize, MaxSize, t);
-            PlatformCollider.size = Vector2.Lerp(minColliderSize, MaxColliderSize, t);
-        }
+        
 
         public void OnTriggerEnter2D(Collider2D other)
         {
-            Debug.Log("Enter");
             UpdateCollisionState(other);
         }
 
         public void OnTriggerStay2D(Collider2D other)
         {
-            Debug.Log("Stay");
             UpdateCollisionState(other);
         }
 
         public void UpdateCollisionState(Collider2D other)
         {
             Vector2 collisionPoint = other.ClosestPoint(transform.position);
-            if (collisionPoint.y >= transform.position.y || collisionPoint.x >= transform.position.x)
+            if (collisionPoint.y >= transform.position.y)
             {
-                playerRobot.RemoveCollisionState(CollisionState.OnPlatformSlope);
+                playerRobot.RemoveCollisionState(collisionState);
                 return;
             }
             
-            if (!playerRobot.CollisionStateActive(CollisionState.OnPlatformSlope) && playerRobot.IsGrounded())
+            switch (SlopeDirection)
+            {
+                case SlopeRotation.Left:
+                    if (collisionPoint.x >= transform.position.x)
+                    {
+                        playerRobot.RemoveCollisionState(collisionState);
+                        return;
+                    }
+                    break;
+                case SlopeRotation.Right:
+                    if (collisionPoint.x <= transform.position.x)
+                    {
+                        playerRobot.RemoveCollisionState(collisionState);
+                        return;
+                    }
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+            
+            if (!playerRobot.CollisionStateActive(collisionState) && playerRobot.IsGrounded())
             {
                 switch (SlopeDirection)
                 {
                     case SlopeRotation.Left:
                         if (rb.velocity.x > 0)
                         {
-                            playerRobot.RemoveCollisionState(CollisionState.OnPlatformSlope);
+                            playerRobot.RemoveCollisionState(collisionState);
                             return;
                         }
                         break;
                     case SlopeRotation.Right:
                         if (rb.velocity.x < 0)
                         {
-                            playerRobot.RemoveCollisionState(CollisionState.OnPlatformSlope);
+                            playerRobot.RemoveCollisionState(collisionState);
                             return;
                         }
                         break;
@@ -92,13 +96,12 @@ namespace Player.Movement
                 }
             }
             
-            playerRobot.AddCollisionState(CollisionState.OnPlatformSlope);
+            playerRobot.AddCollisionState(collisionState);
         }
 
         public void OnTriggerExit2D(Collider2D other)
         {
-            Debug.Log("Exit");
-            playerRobot.RemoveCollisionState(CollisionState.OnPlatformSlope);
+            playerRobot.RemoveCollisionState(collisionState);
         }
     }
 }
--- Assets/Scripts/Player/Robot/PlayerRobot.cs
+++ Assets/Scripts/Player/Robot/PlayerRobot.cs
@@ -47,7 +47,8 @@ namespace Player {
         OnSlope,
         HeadContact,
         OnPlatform,
-        OnPlatformSlope,
+        OnLeftSlopePlatform,
+        OnRightSlopePlatform,
         InFluid,
     }
     
@@ -68,7 +69,8 @@ namespace Player {
         [SerializeField] private SpriteRenderer spriteRenderer;
         [SerializeField] private Rigidbody2D rb;
         [SerializeField] private Collider2D platformCollider;
-        [SerializeField] private Collider2D slopePlatformCollider;
+        [SerializeField] private Collider2D leftSlopePlatformCollider;
+        [SerializeField] private Collider2D rightSlopePlatformCollider;
         [SerializeField] private PlayerDeathScreenUI deathScreenUIPrefab;
         private PolygonCollider2D polygonCollider;
         private bool climbing;
@@ -218,7 +220,7 @@ namespace Player {
         public void AddCollisionState(CollisionState state)
         {
             if (!collisionStates.Add(state)) return;
-            if (state is CollisionState.OnGround or CollisionState.OnSlope or CollisionState.OnPlatform or CollisionState.OnPlatformSlope)
+            if (state is CollisionState.OnGround or CollisionState.OnSlope or CollisionState.OnPlatform or CollisionState.OnLeftSlopePlatform or CollisionState.OnRightSlopePlatform)
             {
                 if (bonusJumps <= 0)
                 {
@@ -459,15 +461,14 @@ namespace Player {
 
         public bool IsGrounded()
         {
-            return CollisionStateActive(CollisionState.OnPlatform) || CollisionStateActive(CollisionState.OnPlatformSlope) || CollisionStateActive(CollisionState.OnGround) || CollisionStateActive(CollisionState.OnSlope);
+            return CollisionStateActive(CollisionState.OnPlatform) || CollisionStateActive(CollisionState.OnGround) || CollisionStateActive(CollisionState.OnSlope) || IsOnSlopedPlatform();
         }
 
-        public bool IsGroundedAndNotOnPlatformSlope()
+        public bool IsOnSlopedPlatform()
         {
-            // Bug central right here
-            return CollisionStateActive(CollisionState.OnPlatform) || CollisionStateActive(CollisionState.OnGround) || CollisionStateActive(CollisionState.OnSlope);
+            return CollisionStateActive(CollisionState.OnLeftSlopePlatform) || CollisionStateActive(CollisionState.OnRightSlopePlatform);
         }
-
+        
         private void FlightMoveUpdate()
         {
             rb.gravityScale = 0;
@@ -707,10 +708,10 @@ namespace Player {
                 jumpEvent = null;
                 return;
             }
-            if ((CollisionStateActive(CollisionState.OnPlatform) || CollisionStateActive(CollisionState.OnPlatformSlope)) && ControlUtils.GetControlKey(PlayerControl.Jump) && ControlUtils.GetControlKey(PlayerControl.MoveDown))
+            if ((CollisionStateActive(CollisionState.OnPlatform) || IsOnSlopedPlatform()) && ControlUtils.GetControlKey(PlayerControl.Jump) && ControlUtils.GetControlKey(PlayerControl.MoveDown))
             {
                 if (CollisionStateActive(CollisionState.OnPlatform)) ignorePlatformFrames = 3;
-                if (CollisionStateActive(CollisionState.OnPlatformSlope))
+                if (IsOnSlopedPlatform())
                 {
                     liveYUpdates = 3;
                     ignoreSlopePlatformFrames = 3;
@@ -861,10 +862,11 @@ namespace Player {
             {
                 rb.drag = defaultLinearDrag;
             }
-            
-            platformCollider.enabled = ignorePlatformFrames < 0 && rb.velocity.y < 0.01f && !collisionStates.Contains(CollisionState.OnPlatformSlope);
-            slopePlatformCollider.enabled = ignoreSlopePlatformFrames < 0 && collisionStates.Contains(CollisionState.OnPlatformSlope) && !(ControlUtils.GetControlKey(PlayerControl.MoveDown) && collisionStates.Contains(CollisionState.OnPlatform));
-            
+
+            platformCollider.enabled = ignorePlatformFrames < 0 && rb.velocity.y < 0.01f;
+            bool ignoreSlopedPlatforms = ControlUtils.GetControlKey(PlayerControl.MoveDown) && collisionStates.Contains(CollisionState.OnPlatform);
+            leftSlopePlatformCollider.enabled = ignoreSlopePlatformFrames < 0 && collisionStates.Contains(CollisionState.OnLeftSlopePlatform) && !ignoreSlopedPlatforms;
+            rightSlopePlatformCollider.enabled = ignoreSlopePlatformFrames < 0 && collisionStates.Contains(CollisionState.OnRightSlopePlatform) && !ignoreSlopedPlatforms;
             bool grounded = IsGrounded();
             animator.SetBool(Air,coyoteFrames < 0 && !grounded);
             if (grounded)
@@ -910,7 +912,7 @@ namespace Player {
 
             if (liveYUpdates > 0) return FREEZE_Z;
             
-            if (CollisionStateActive(CollisionState.OnSlope) || CollisionStateActive(CollisionState.OnPlatformSlope))
+            if (CollisionStateActive(CollisionState.OnSlope) || IsOnSlopedPlatform())
             {
                 bool moving = ControlUtils.GetControlKey(PlayerControl.MoveLeft) || ControlUtils.GetControlKey(PlayerControl.MoveRight);
                 bool touchingWall = CollisionStateActive(CollisionState.OnWallLeft) || CollisionStateActive(CollisionState.OnWallRight);
