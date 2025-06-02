9951043f96b757e01067f89bcbf05ef8845f9173
Isaac-Stamatelakis <97195929+Isaac-Stamatelakis@users.noreply.github.com>
2025-06-02 11:40:41 -0600

    Added EditorTileItemRebuilder, currently testing pushing incase deletes shit its not supposed to

---
+++ Assets/Editor/Item/EditorTileItemRebuilder.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class EditorTileItemRebuilder
+{
+    private string tileName;
+    private TileItem tileItem;
+
+    public void Locate()
+    {
+        string tileId = tileName.ToLower().Replace(" ", "_");
+        string[] guids = AssetDatabase.FindAssets("t:" + nameof(TileItem));
+        foreach (string guid in guids)
+        {
+            string path = AssetDatabase.GUIDToAssetPath(guid);
+            TileItem searchItem = AssetDatabase.LoadAssetAtPath<TileItem>(path);
+            if (!string.Equals(tileItem?.id, tileId)) continue;
+            tileItem = searchItem;
+            break;
+        }
+    }
+
+    public bool Found()
+    {
+        return tileItem;
+    }
+
+    public string GetItemPath()
+    {
+        return !Found() ? string.Empty : AssetDatabase.GetAssetPath(tileItem);
+    }
+
+    public void DeleteOldTileAssets()
+    {
+        string path = AssetDatabase.GetAssetPath(tileItem);
+        string itemFolderPath = Path.GetDirectoryName(path);
+        if (string.IsNullOrEmpty(itemFolderPath))
+        {
+            Debug.LogError("Could not find item path");
+            return;
+        }
+        
+        TileBase oldTile = tileItem.tile;
+        if (oldTile)
+        {
+            AssetDatabase.DeleteAsset(AssetDatabase.GetAssetPath(oldTile));
+        }
+        
+        const string SPRITE_FOLDER_NAME = "Sprites";
+        string spriteFolderPath = Path.Combine(itemFolderPath, SPRITE_FOLDER_NAME);
+        if (AssetDatabase.IsValidFolder(spriteFolderPath)) return;
+        string[] assetGuids = AssetDatabase.FindAssets("", new[] { spriteFolderPath });
+        
+        foreach (string guid in assetGuids)
+        {
+            string assetPath = AssetDatabase.GUIDToAssetPath(guid);
+            AssetDatabase.DeleteAsset(assetPath);
+        }
+    }
+
+    public void ReplaceTile(TileBase tileBase)
+    {
+        tileItem.tile = tileBase;
+        EditorUtility.SetDirty(tileItem);
+        AssetDatabase.SaveAssetIfDirty(tileItem);
+        AssetDatabase.Refresh();
+    }
+}
