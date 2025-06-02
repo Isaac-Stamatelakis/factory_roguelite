f39c428aa21c6577d97256466dd23c71bf06483f
Isaac-Stamatelakis <97195929+Isaac-Stamatelakis@users.noreply.github.com>
2025-06-02 15:13:38 -0600

    Background auto tile placement can now place inside platforms, refactored some platform code

--- Assets/Scripts/Player/Mouse/TilePlaceSearcher/BaseTilePlacementSearcher.cs
+++ Assets/Scripts/Player/Mouse/TilePlaceSearcher/BaseTilePlacementSearcher.cs
@@ -46,6 +46,79 @@ namespace Player.Mouse.TilePlaceSearcher
         }
     }
 
+    public static class TileSearchUtils
+    {
+        public static Vector2? BresenhamLine(Vector2 origin, Vector2 direction, Vector2 mousePosition, List<IWorldTileMap> interactableTilemaps, float maxDistance, bool placeableInPlatforms)
+        {
+            if (interactableTilemaps.Count == 0) return null;
+            
+            Vector2? lastEmptyTile = null;
+            bool tileFound = false;
+            float distance = Mathf.Min((origin - mousePosition).magnitude, maxDistance);
+            
+            Tilemap tilemap = interactableTilemaps[0].GetTilemap(); // Doesn't matter what map it is all are the same for getting cell
+            Vector3Int startCell = tilemap.WorldToCell(origin);
+            Vector2 endPos = origin + direction * distance;
+            Vector3Int endCell = tilemap.WorldToCell(endPos);
+            
+            int x0 = startCell.x;
+            int y0 = startCell.y;
+            int x1 = endCell.x;
+            int y1 = endCell.y;
+            
+            int dx = Mathf.Abs(x1 - x0);
+            int dy = Mathf.Abs(y1 - y0);
+            int sx = x0 < x1 ? 1 : -1;
+            int sy = y0 < y1 ? 1 : -1;
+            int err = dx - dy;
+            
+            while (true)
+            {
+                Vector2Int currentCell = new Vector2Int(x0, y0);
+
+                bool found = false;
+                foreach (IWorldTileMap worldTileMap in interactableTilemaps)
+                {
+                    if (worldTileMap.HasTile(currentCell))
+                    {
+                        tileFound = true;
+                        found = true;
+                        if (placeableInPlatforms && worldTileMap.GetTileMapType() == TileMapType.Platform)
+                        {
+                            lastEmptyTile = new Vector2(currentCell.x * Global.TILE_SIZE, currentCell.y * Global.TILE_SIZE);;
+                        }
+                        break;
+                    }
+                }
+
+                if (!found)
+                {
+                    lastEmptyTile = new Vector2(currentCell.x * Global.TILE_SIZE, currentCell.y * Global.TILE_SIZE);
+                    if (tileFound)
+                    {
+                        return lastEmptyTile;
+                    }
+                }
+                
+                if (x0 == x1 && y0 == y1) break;
+    
+                int e2 = 2 * err;
+                if (e2 > -dy)
+                {
+                    err -= dy;
+                    x0 += sx;
+                }
+                if (e2 < dx)
+                {
+                    err += dx;
+                    y0 += sy;
+                }
+            }
+
+            return lastEmptyTile;
+        }
+    }
+
     public class BackgroundBaseTilePlacementSearcher : BaseTilePlacementSearcher
     {
         private List<IWorldTileMap> collidableMaps;
@@ -66,76 +139,9 @@ namespace Player.Mouse.TilePlaceSearcher
             float theta = Mathf.Atan2(mousePosition.y-PlayerScript.transform.position.y, mousePosition.x-PlayerScript.transform.position.x);
             
             Vector2 mouseDirection = new Vector2(Mathf.Cos(theta),Mathf.Sin(theta));
-            Vector2? foundTile = CheckCellsWithBresenham(PlayerScript.transform.position, mouseDirection);
+            Vector2? foundTile = TileSearchUtils.BresenhamLine(PlayerScript.transform.position, mouseDirection,mousePosition,collidableMaps,5f,true);
             
-            if (!foundTile.HasValue) return mousePosition;
-           
-            return foundTile.Value;
-            
-            Vector2? CheckCellsWithBresenham(Vector2 origin, Vector2 direction)
-            {
-                Vector2? lastEmptyTile = null;
-                Vector2? foundTile = null;
-                float distance = Mathf.Min((origin - mousePosition).magnitude, 5);
-                
-                Tilemap tilemap = collidableMaps[0].GetTilemap(); // Doesn't matter what map it is all are the same for getting cell
-                Vector3Int startCell = tilemap.WorldToCell(origin);
-                Vector2 endPos = origin + direction * distance;
-                Vector3Int endCell = tilemap.WorldToCell(endPos);
-                
-                int x0 = startCell.x;
-                int y0 = startCell.y;
-                int x1 = endCell.x;
-                int y1 = endCell.y;
-                
-                int dx = Mathf.Abs(x1 - x0);
-                int dy = Mathf.Abs(y1 - y0);
-                int sx = x0 < x1 ? 1 : -1;
-                int sy = y0 < y1 ? 1 : -1;
-                int err = dx - dy;
-                
-                while (true)
-                {
-                    Vector2Int currentCell = new Vector2Int(x0, y0);
-
-                    bool found = false;
-                    foreach (IWorldTileMap worldTileMap in collidableMaps)
-                    {
-                        if (worldTileMap.HasTile(currentCell))
-                        {
-                            foundTile = new Vector2(currentCell.x * Global.TILE_SIZE, currentCell.y * Global.TILE_SIZE);
-                            found = true;
-                            break;
-                        }
-                    }
-
-                    if (!found)
-                    {
-                        lastEmptyTile = new Vector2(currentCell.x * Global.TILE_SIZE, currentCell.y * Global.TILE_SIZE);
-                        if (foundTile.HasValue)
-                        {
-                            return lastEmptyTile;
-                        }
-                    }
-                    
-                    if (x0 == x1 && y0 == y1) break;
-        
-                    int e2 = 2 * err;
-                    if (e2 > -dy)
-                    {
-                        err -= dy;
-                        x0 += sx;
-                    }
-                    if (e2 < dx)
-                    {
-                        err += dx;
-                        y0 += sy;
-                    }
-                }
-
-                return lastEmptyTile;
-            }
-            
+            return foundTile ?? mousePosition;
         }
     }
 }
