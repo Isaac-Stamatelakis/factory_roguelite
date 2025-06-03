a223c6b3eeb6081b9496c1e7b7452663574bf094
Isaac-Stamatelakis <97195929+Isaac-Stamatelakis@users.noreply.github.com>
2025-06-03 13:42:42 -0600

    Added editor window with functionality for locating User Interface Materials. Currently testing adding properties to them

--- Assets/Editor/Material/UIMaterialPropertyFixerWindow.cs
+++ Assets/Editor/Material/UIMaterialPropertyFixerWindow.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEditor;
 using UnityEngine;
 
 public class UIMaterialPropertyFixerWindow : EditorWindow
 {
     [MenuItem("Tools/Misc/UIMaterial Properties")]
+    
+    
     public static void ShowWindow()
     {
         UIMaterialPropertyFixerWindow window = (UIMaterialPropertyFixerWindow)EditorWindow.GetWindow(typeof(UIMaterialPropertyFixerWindow));
         window.titleContent = new GUIContent("UI Material Property Assigner");
     }
-
+    private const string SPRITE_LIT_KEYWORD = "USE_SHAPE_LIGHT_TYPE_0";
     void OnGUI()
     {
         if (GUILayout.Button("Apply Properties"))
@@ -23,10 +26,37 @@ public class UIMaterialPropertyFixerWindow : EditorWindow
     void ApplyProperties()
     {
         string[] guids = AssetDatabase.FindAssets("t:" + nameof(Material));
+        int count = 0;
         foreach (string guid in guids)
         {
-            Material material = AssetDatabase.LoadAssetAtPath<Material>(AssetDatabase.GUIDToAssetPath(guid));
-            if (material.)
+            string assetPath = AssetDatabase.GUIDToAssetPath(guid);
+            if (!assetPath.StartsWith("Assets/Material/UI") && !assetPath.StartsWith("Assets/Material/Items"))
+            {
+                continue;
+            }
+            Material material = AssetDatabase.LoadAssetAtPath<Material>(assetPath);
+            if (IsSpriteLitShader(material)) continue;
+            var shader = material.shader;
+            string shaderPath = AssetDatabase.GetAssetPath(shader);
+            if (!shaderPath.EndsWith(".shadergraph")) continue;
+            string json = File.ReadAllText(shaderPath);
+            Debug.Log(json);
+            break;
+            Debug.Log(shader.name);
+        }
+
+        bool IsSpriteLitShader(Material material)
+        {
+            if (!material.shader) return false;
+            foreach (string keyword in material.shader.keywordSpace.keywordNames)
+            {
+                if (keyword == SPRITE_LIT_KEYWORD)
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
