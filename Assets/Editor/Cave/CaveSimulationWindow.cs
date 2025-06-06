86ef6f5634b1586d9fb20dae898ce012dc4ed333
Isaac-Stamatelakis <97195929+Isaac-Stamatelakis@users.noreply.github.com>
2025-06-06 10:27:29 -0600

    Fixed bug where player sometimes got stuck on the top of platforms

--- Assets/Editor/Cave/CaveSimulationWindow.cs
+++ Assets/Editor/Cave/CaveSimulationWindow.cs
@@ -13,7 +13,7 @@ using WorldModule.Caves;
 public class CaveSimulationWindow : EditorWindow
 {
     private int simulationCount = 10;
-    private List<CaveObject> caves;
+    private List<GenerationModel> genModels;
     [MenuItem("Tools/Cave/Simulation Runner")]
     public static void ShowWindow()
     {
@@ -23,13 +23,13 @@ public class CaveSimulationWindow : EditorWindow
 
     public void OnEnable()
     {
-        caves = new List<CaveObject>();
+        genModels = new List<GenerationModel>();
         string[] guids = AssetDatabase.FindAssets("t:" + nameof(CaveObject));
         foreach (string guid in guids)
         {
             CaveObject caveObject = AssetDatabase.LoadAssetAtPath<CaveObject>(AssetDatabase.GUIDToAssetPath(guid));
-            if (!caveObject) continue;
-            caves.Add(caveObject);
+            if (!caveObject?.generationModel) continue;
+            genModels.Add(caveObject.generationModel);
         }
     }
 
@@ -37,7 +37,7 @@ public class CaveSimulationWindow : EditorWindow
     void OnGUI()
     {
         GUI.enabled = false;
-        GUILayout.TextArea($"Found {caves.Count} Caves. Current simulation count {simulationCount}");
+        GUILayout.TextArea($"Found {genModels.Count} Generation Models. Current simulation count {simulationCount}, estimated completion time {genModels.Count * simulationCount * 0.4f}s.");
         GUI.enabled = true;
         if (GUILayout.Button("Run Simulations"))
         {
@@ -47,17 +47,30 @@ public class CaveSimulationWindow : EditorWindow
 
     void Simulate()
     {
-        List<GenerationModel> generationModels = new List<GenerationModel>();
-        foreach (CaveObject caveObject in caves)
+        foreach (GenerationModel generationModel in genModels)
         {
-            GenerationModel generationModel = caveObject.generationModel;
             if (!generationModel) continue;
+            GenerationModel.CaveSimulationResults total = new GenerationModel.CaveSimulationResults(0,0);
+            for (int i = 0; i < simulationCount; i++)
+            {
+                var simulationResult = SimulateResults(512, 512, generationModel);
+                total.EstimatedDecorationSpawnLocationRatio += simulationResult.EstimatedDecorationSpawnLocationRatio;
+                total.EstimatedResultFillRatio += simulationResult.EstimatedResultFillRatio;
+            }
+
+            total.EstimatedDecorationSpawnLocationRatio /= simulationCount;
+            total.EstimatedResultFillRatio /= simulationCount;
+            generationModel.SimulationResults = total;
+            EditorUtility.SetDirty(generationModel);
+            AssetDatabase.SaveAssetIfDirty(generationModel);
+            Debug.Log($"Simulation of {generationModel.name} complete");
         }
-        Debug.Log($"loaded {generationModels.Count}");
+        Debug.Log($"Simulation of '{genModels.Count}' models complete");
     }
     
-    private GenerationModel.CaveSimulationResults SimulateResults(int[][] grid, int width, int height, GenerationModel generationModel)
+    private GenerationModel.CaveSimulationResults SimulateResults(int width, int height, GenerationModel generationModel)
     {
+        int[][] grid = generationModel.GenerateGridInstant(UnityEngine.Random.Range(int.MinValue, int.MaxValue),new Vector2Int(width,height));
         int emptyCount = 0;
         int decorationCount = 0;
         int[] dx = { -1, 1, 0, 0 };
